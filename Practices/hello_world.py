5d19c75af5769a0ff809df188b9b468141bb257a
Valerie5801 <valerie.yang@ucas-edu.net>
2025-08-20 10:46:23 -0600

    Working on Advanced Hello World.

---
+++ Practices/hello_world.py
@@ -0,0 +1,9 @@
+# VY 2nd Hello World First Practice
+
+name = input("What's your name?: ")
+existingUser = ["Sunny", "Aubrey", "Kel", "Hero", "Basil", "Valerie", "Ms. LaRose"]
+
+if name == existingUser[0-6]:
+    print("Welcome back!")
+else:
+    print("Welcome new user!")
