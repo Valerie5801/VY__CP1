a2c91c9bc3d5f41d3166dd831b62cc06b5b3f308
Valerie5801 <valerie.yang@ucas-edu.net>
2025-08-20 10:21:35 -0600

    Created an example document for how to create inputs in two ways.

---
+++ notes/inputs.py
@@ -0,0 +1,11 @@
+#VY 2nd Inputs and outputs notes
+
+color = input("What's your favorite color?: ")
+print(color, "is very cool!")
+
+
+#other way to print an input
+
+print("What's another color you like?: ")
+colorTwo = input()
+print(colorTwo, "is also very pretty.")
