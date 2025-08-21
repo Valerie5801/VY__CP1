029726ba1b585c1d01635fea988a3b20b169cc01
Valerie5801 <valerie.yang@ucas-edu.net>
2025-08-21 10:51:09 -0600

    Added things to the Variables Notes.

---
+++ notes/variables.py
@@ -0,0 +1,14 @@
+# VY 2nd Variables Notes
+
+#defining a variable
+
+students_count = 29
+amount_water = "1oz"
+name = "Jake"
+
+
+grade = 99
+print(grade)
+#resetting a variable
+grade = 88
+print(grade)
