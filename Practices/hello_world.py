bc570d521bdba10ef2d69de2c679e6af8f0b0ac1
Valerie5801 <valerie.yang@ucas-edu.net>
2025-08-20 20:40:40 -0600

    Completed "Hello World" advanced level.

--- Practices/hello_world.py
+++ Practices/hello_world.py
@@ -1,9 +1,11 @@
 # VY 2nd Hello World First Practice
-
+existingUser = ["Sunny", "Aubrey", "Kel", "Hero", "Basil"]
+admin = ["Valerie", "Ms.LaRose"]
 name = input("What's your name?: ")
-existingUser = ["Sunny", "Aubrey", "Kel", "Hero", "Basil", "Valerie", "Ms. LaRose"]
 
-if name == existingUser[0-6]:
+if name in admin:
+    print("Greetings, admin.")
+elif name in existingUser:
     print("Welcome back!")
 else:
     print("Welcome new user!")
