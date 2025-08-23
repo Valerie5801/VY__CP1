bc092d2e15e404bc04c69a9bafae20df5a4d9161
Valerie5801 <valerie.yang@ucas-edu.net>
2025-08-23 11:43:17 -0600

    Finished the Who Are You Project advanced level.

--- Projects/who_are_you.py
+++ Projects/who_are_you.py
@@ -1,15 +1,22 @@
 #VY 2nd Who Are You
 
-name = input("What is your name?: ")
-userAge = input("How old are you?: ")
-color = input("What's your favorite color?: ")
-nameList = [name]
+allInfo = {}
+userInfo = {}
 
-print("So, ", name, ", you're ", userAge, "years old, and you like the color", color, "? It's nice to meet you!")
 
-newName = input("What is your name?: ")
-if newName in name:
-     name = newName
-     print("hi")
-else:
-     print("i think youre new")
+while True:
+     name = input("What is your name?(type 'quit' to stop): ")
+     if name.lower() == 'quit': #terminating the program
+          print("Goodbye.")
+          break
+     elif name in userInfo: #recognizing a name
+          recognizedInfo = userInfo[name]
+          print(f"Welcome back, {name}! You were {recognizedInfo['age']} years old and your favorite color was {recognizedInfo['color']} last time.")
+     else: #saving a new user's information
+          userAge = input(f"How old are you, {name}?: ")
+          color = input("What's your favorite color?: ")
+
+          userInfo[name] = {"age":userAge, "color":color}
+          allInfo["user"] = userInfo
+
+          print("So, ", name, ", you're ", userAge, "years old, and you like the color", color, "? It's nice to meet you!")
