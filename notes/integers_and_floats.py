e608ef88e723af73edd7354332340825bdbb6740
Valerie5801 <valerie.yang@ucas-edu.net>
2025-08-28 10:24:57 -0600

    Finished Integer and Floats notes

--- notes/integers_and_floats.py
+++ notes/integers_and_floats.py
@@ -12,3 +12,15 @@ enjoyment = 0.01
 
 #math
 print(8/4 +7 - (8+2)% 4 * 10**3)
+
+#Changing data types
+age = int(input("How old are you?\n"))
+
+print("wow youre old. you are", 525600*age, "minutes old :O")
+
+happy_time = float(input("How long were you happy?: "))
+print("you look like you were happy", happy_time/2, "hours are you sure youre okay")
+
+#rounding
+not_pi = 3.14159265358979
+print(round(not_pi, 3))
