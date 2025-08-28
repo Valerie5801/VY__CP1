227c49264268a025d06f54c5e4f0c44132d9061c
Valerie5801 <valerie.yang@ucas-edu.net>
2025-08-28 10:51:10 -0600

    Started the Average Grade Project

---
+++ Practices/average_grade.py
@@ -0,0 +1,33 @@
+#VY 2nd Average Grade
+
+print("I can help you calculate the average of all your classes.")
+total_classes = {}
+
+amount_classes = int(input("How many classes do you have?"))
+counter = 1
+
+#class_1 = input("What is your first class?: ")
+#class_2 = input("What is your second class?: ")
+#class_3 = input("What is your third class?: ")
+#class_4 = input("What is your fourth class?: ")
+#class_5 = input("What is your fifth class?: ")
+#class_6 = input("What is your sixth class?: ")
+#class_7 = input("What is your seventh class?: ")
+
+while counter <= amount_classes:
+    class_name = input("What's a class you have?(type 'quit' to stop): ")
+    if 
+    user_class = float(input(""))
+
+
+#pe = float(input("What is your grade in P.E.?: "))
+#computer_science = float(input("What is your grade in Computer Science?: "))
+#math = float(input("What is your grade for Math?: "))
+#english = float(input("What is your grade in English?: "))
+#chemistry = float(input("What is your grade in Chemistry?: "))
+#health = float(input("What is your grade in Health?: "))
+
+#total = pe + computer_science + math + english + chemistry + health
+#average = total/7
+
+#print("Your average grade across all classes is", round(average, 2))
