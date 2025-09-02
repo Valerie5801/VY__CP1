da1ba4221874a63449b66b78ce763f6cdeeac0bc
Valerie5801 <valerie.yang@ucas-edu.net>
2025-09-02 10:51:03 -0600

    Nearly finished Strings notes

--- notes/strings.py
+++ notes/strings.py
@@ -1,2 +1,36 @@
 #VY 2nd String Notes
 
+#example
+sentence = "The quick brown fox has failed to jump over the lazy dog."
+first_name = 'Aubrey'
+last_name = 'Aubergine'
+month = "September"
+food = "cold steak"
+
+#using ' or "
+name = "La'Rose" #use the OTHER kind of quotes so the computer doesn't close the string
+wonderhoy = 'i like saying "wonderhoy"'
+
+
+
+#getting the length of a string
+length = len(month)
+
+print("There are", length, "characters in the word September.")
+
+
+
+#Escape character
+print('\tPlease, \ndon\'t die.')
+
+
+
+#concatenation
+full_name = first_name + " " + last_name
+print(full_name)
+
+#Index
+
+
+#slice
+print(last_name[5:9])
