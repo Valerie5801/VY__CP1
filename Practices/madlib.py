ca40036191baec4dcda21b27733d57841cf6369f
Valerie5801 <valerie.yang@ucas-edu.net>
2025-09-03 10:50:56 -0600

    Finished the Madlib practice

---
+++ Practices/madlib.py
@@ -0,0 +1,23 @@
+#VY 2nd Madlib
+
+year = input("Give me a four digit number: ")
+country = input("Give me a country: ")
+vehicle = input("Give me a vehicle: ")
+ship_name = input("Give me a name: ")
+pilots = input("Give me a plural noun: ")
+action = input('Give me a verb ending in "ing": ')
+profession = input("Give me a plural noun: ")
+city = input("Give me a city: ")
+jets = input("Give me a noun: ")
+desc_sky = input("Give me an adjective: ")
+shape_sky = input("Give me a plural noun: ")
+altitude = input("Give me a number: ")
+object_sky = input("Give me a noun: ")
+verb_people = input("Give me a verb: ")
+orbits = input("Give me a number: ")
+adverb = input("Give me an adverb: ")
+station = input("Give me a noun: ")
+result_adj = input("Give me an adjective: ")
+
+madlib = "In " + year + ", the " + country + " space station launched the first real Space " + vehicle + ". It was named the " + ship_name + " and it was piloted by a team of brave " + pilots + ". They had practiced " + action + " for two years and were experts at " + profession + ". \n" + ship_name + " took off from " + city + " using its powerful first-stage " + jets + " and soared off into the " + desc_sky + " blue " + shape_sky + ". At an altitude of " + altitude + " feet, it went into orbit around the " + object_sky + ". It was an amazing sight to " + verb_people + " from " + country + ". After " + orbits + " orbits, the " + vehicle + " landed " + adverb + " at an air force " + station + ". It was another " + result_adj + " day for the " + country + " space program."
+print(madlib)
