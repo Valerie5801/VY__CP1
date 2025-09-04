3804abfef66d7c7526d07fda70a83fa8e7a2b01d
Valerie5801 <valerie.yang@ucas-edu.net>
2025-09-04 10:44:02 -0600

    finished the Strings Methods notes

---
+++ notes/strings_methods.py
@@ -0,0 +1,23 @@
+#VY 2nd Strings Methods Notes
+
+#Methods
+name = input("whats ur name: ").strip().title() #can use multiple methods
+#.lower makes all text lowercase
+#.upper makes all text uppercase
+#.capitalize makes only the first letter uppercase
+#.title makes all first letters of words uppercase
+print("hello {} hiiiii".format(name))
+
+print(f"hello {name} hiiiii")
+#age = input("ok now how old are you: ").isdigit()
+#print(age)
+
+#.find()
+#sentence = "The quick brown fox jumps over the lazy dog!"
+#word = "brown"
+#start= sentence.find(word)
+#length = len(word)
+
+#print(sentence[start:start+length])
+
+#formatting
