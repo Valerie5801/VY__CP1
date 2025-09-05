c9cea5c67864320e60bf0fa4735c0aaf76d20760
Valerie5801 <valerie.yang@ucas-edu.net>
2025-09-05 10:49:47 -0600

    Continued idiot proof

--- Practices/idiot_proof.py
+++ Practices/idiot_proof.py
@@ -1,14 +1,33 @@
 #VY 2nd Idiot Proof
 
-full_name = input("What is your full name?: ")
-phone_number = int(input("What is your phone number?(don't use dashes or spaces): "))
-gpa = input("What is your GPA?: ")
+while True:
+    full_name = input("What is your full name?: ")
+    phone_number = input("What is your 9 digit phone number?(don't use dashes or spaces): ")
+    gpa = input("What is your GPA?: ")
 
-check_name = full_name.isalpha()
-check_number = phone_number.isdigit()
-check_gpa = gpa.isdecimal()
+    check_name = full_name.isalpha()
+    check_number = phone_number.isdigit()
+    check_gpa = gpa.isdecimal()
 
-if check_name == False:
-    full_name = full_name.strip().title()
+    while check_name == False:
+        if check_name == True:
+            full_name = full_name.strip().title()
+            break
+    else:
+        print("invalid name")
 
-print(f"Your name is {full_name}, your phone number is {phone_number}, and your GPA is currently {gpa}.")
+    while check_number == False:
+        if check_number == True:
+            phone_number = int(phone_number)
+            break
+    else:
+        print("thats not a phone number")
+
+    while check_gpa == False:
+        if check_gpa == True:
+            gpa = round(float(gpa), 1)
+            break
+    else:
+        print("wah")
+
+    print(f"Your name is {full_name}, your phone number is {phone_number}, and your GPA is currently {gpa}.")
