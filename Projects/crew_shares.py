aab53d0e4b33680521858c4e9a472378d7eb7735
Valerie5801 <valerie.yang@ucas-edu.net>
2025-09-13 07:48:54 -0600

    Finished Crew Shares project

--- Projects/crew_shares.py
+++ Projects/crew_shares.py
@@ -1,20 +1,20 @@
 #VY 2nd Crew Shares
 
-num_crew = int(input("A crew is sailing on a ship this evening. How many members are there?: "))
-total_money = round(float(input("They have earned a large sum of money! How much money did they earn: ")), 2)
+num_crew = int(input("A crew is sailing on a ship this evening. How many members are there?(not including captain or first mate): "))
+total_money = round(float(input("They have earned a large sum of money! How much money did they earn?: ")), 2)
 
-amount_shares = num_crew + 7 + 3 - 2 #the reason i add 7 and 3 shares is for the captain and the first mate respectively. I subtracted two because I removed their singular share.
-share_value = total_money / num_crew
+amount_shares = num_crew + 7 + 3
+share_value = total_money / amount_shares
 cap_amount = share_value * 7
 first_amount = share_value * 3
 crew_mem = share_value - 500
 
-print(f"The crew earned ${total_money:.2} that night.")
-print(f"The crew is made up of {num_crew} members.")
+print(f"The crew earned ${total_money:.2f} that night.")
+print(f"The crew is made up of {num_crew} members.\n")
 
 print("The captain didn't have enough time to equally divvy up all of the money before releasing everyone to port. \nHe decided to give each crew member $500, and then sat down with his first mate to discuss.")
 
-print(f"After the captain and the first mate divvied up all of the money to each of the crew...")
-print(f"The captain gets ${cap_amount:.2}")
-print(f"The first mate gets ${first_amount:.2}")
-print(f"And each member of the crew still needs ${crew_mem:.2}")
+print(f"After the captain and the first mate divvied up all of the money to each of the crew...\n")
+print(f"The captain gets ${cap_amount:.2f}")
+print(f"The first mate gets ${first_amount:.2f}")
+print(f"And each member of the crew still needs ${crew_mem:.2f}")
