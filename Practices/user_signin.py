7cd79e1a5cdd9b36829a51db43edd1cf742468a4
Valerie5801 <valerie.yang@ucas-edu.net>
2025-09-19 10:50:07 -0600

    started the User sign in practice

--- Practices/user_signin.py
+++ Practices/user_signin.py
@@ -1,2 +1,15 @@
 #VY 2nd User Sign in
 
+username = "OMORI" #setting username and password
+password = "somethingisbehindyou"
+
+name_input = input("What is your username?: ") #getting user inputs
+pass_input = input("What is your password?:")
+
+if username == "OMORI": #checking for username
+    if password == "somethingisbehindyou": #if the username is the same, checking for password
+        print(f"Welcome back, {username}")
+    else:
+        print("You were wrong. Go back")
+else:
+    print("I dont recognize that username.")
