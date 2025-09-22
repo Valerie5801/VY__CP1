2937d9fe48649f2fe2cc2766246679ce9e295170
Valerie5801 <valerie.yang@ucas-edu.net>
2025-09-22 10:51:12 -0600

    Finished advanced User signin

--- Practices/user_signin.py
+++ Practices/user_signin.py
@@ -1,15 +1,31 @@
 #VY 2nd User Sign in
 
-username = "OMORI" #setting username and password
-password = "somethingisbehindyou"
+users = [["OMORI", "somethingisbehindyou"], ["Aubrey", "itmeanteverything"], ["Kel", "threedaysleft"]]
 
-name_input = input("What is your username?: ") #getting user inputs
-pass_input = input("What is your password?:")
-
-if username == "OMORI": #checking for username
-    if password == "somethingisbehindyou": #if the username is the same, checking for password
-        print(f"Welcome back, {username}")
+while True:
+    name_input = input("What is your username?: ")
+    pass_check = input("What is your password?: ")
+    if name_input in users[0][0] or name_input in users[1][0] or name_input in users[2][0]:
+        if pass_check in users[0][1] or pass_check in users[1][1] or pass_check in users[2][1]:
+            print("Welcome back!")
+            break
+        else:
+            print("Your password is wrong")
+            break
     else:
-        print("You were wrong. Go back")
-else:
-    print("I dont recognize that username.")
+        print("I don't recognize your username.")
+        break
+
+
+#username = "OMORI" #setting username and password
+#password = "somethingisbehindyou"
+
+#name_input = input("What is your username?: ")
+#if name_input == username: #checking for username
+ #   pass_input = input("What is your password?:")
+  #  if pass_input == password: #if the username is the same, checking for password
+   #     print(f"Welcome back, {username}")
+   # else:
+    #    print("You were wrong. Go back")
+#else:
+ #   print("I dont recognize that username.")
