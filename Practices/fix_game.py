dcb8f14895c10add039421fb17cd37415e0726e0
Valerie5801 <valerie.yang@ucas-edu.net>
2025-09-27 10:08:52 -0600

    Removed extra file and finished the fix game practice.

--- Practices/fix_game.py
+++ Practices/fix_game.py
@@ -1,4 +1,4 @@
-#VY 2nd Fix the Game
+#VY 2nd Fix the game
 
 import random
 def start_game():
@@ -9,24 +9,26 @@ def start_game():
     attempts = 0
     game_over = False
     while not game_over:
-        #There's another issue with guess = int(input("Enter your guess: ")) being on this line: even if the user used all of their attempts, they can guess one more time but the program will only say that the user used all attempts. This is a logic error.
+        #guess = input("Enter your guess: ")
+        #The input should be converted to an integer for it to be able to compare with the chosen number. This is a run-time error because it crashes the program when it tries to compare a string to an integer.
+        guess= int(input("Enter your guess: "))
         if attempts >= max_attempts:
             print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
             game_over = True
-        else:
-            guess = input("Enter your guess: ") #The user input should be a number, but instead this input defaults to a string. When the computer tries to compare a string to a number, it breaks. This is a run-time error.
-            if not guess.isdigit(): #This is a fail safe.
-                print("Invalid input. Please put an integer.")
-            else:
-                if guess == number_to_guess: #This "if" statement (and all elifs below it) is suppoesd to be inside the above else statement because it should only run if the user still has attempts left. This is a logic error. This is a problem since even if the user loses the game, the program will still print "Too low" or "too high" before printing "Game over". This can quickly become annoying.
-                    print("Congratulations! You've guessed the number!")
-                    game_over = True
-                elif guess > number_to_guess:
-                    print("Too high! Try again.")
-                    attempts += 1 #(This also applies to the conditional below this one.) attempts += 1 must run after every time the user guesses incorrectly so there is a way for the user to lose. If attempts never reaches the max_attempts, there is no way that if attempts >= max_attempts will be true, so the user basically has unlimited attempts. This is a logic error.
-                elif guess < number_to_guess:
-                    print("Too low! Try again.")
-                    attempts += 1
-        continue
+        #if guess == number_to_guess:
+         #   print("Congratulations! You've guessed the number!")
+          #  game_over = True
+          #This if statement should be moved to the end and made as an elif statement attached to the if statement above this one. This is so it won't print Too high or too low when the player runs out of attempts (all one conditional). This is a logic error.
+        elif guess > number_to_guess:
+            print("Too high! Try again.")
+            attempts += 1 #This must be added so that attempts>= max_attempts can be reached so the user will not have infinite attempts. This is a logic error.
+        elif guess < number_to_guess:
+            print("Too low! Try again.") 
+            attempts += 1 #This must be added so that attempts>= max_attempts can be reached so the user will not have infinite attempts. This is a logic error.
+        elif guess == number_to_guess:
+            print("Congratulations! You've guessed the number!")
+            game_over = True 
+        #continue
+        #This continue statement is not needed as it does not change anything and is therefore redundant. This is a logic error.
     print("Game Over. Thanks for playing!")
 start_game()
