88c38c5b7c49e518d2a0459c0efab4e2173805ac
Valerie5801 <valerie.yang@ucas-edu.net>
2025-09-30 10:08:41 -0600

    Finished the List Manager practice

--- Practices/list_manager.py
+++ Practices/list_manager.py
@@ -1,16 +1,28 @@
-#VY 2nd Shopping Lits Manager
+#VY 2nd Shopping List Manager
+
+shop_list = []
 
 while True:
-    action = input("What would you like to do?(view, add, remove, exit): ")
+    action = input("What would you like to do with your list?(view, add, remove, exit): ")
     #Write your code here
     if action.strip().lower() == "view":
-        print("yum")
+        if shop_list:
+            print(f"Here is your current list: {shop_list}")
+        else:
+            print("List not found.")
     elif action.strip().lower() == "add":
-        print("wonderhoy")
+        task = input("Enter your new task here: ")
+        shop_list.append(task)
+        print(f'"{task}" has been successfully added to your list.')
     elif action.strip().lower() == "remove":
-        print("oyasumi")
-    elif action == "exit":
-        print("k bye")
+        task = input("Enter the task you would like to remove: ")
+        if task in shop_list:
+            shop_list.remove(task)
+            print(f'"{task}" has been successfully removed from your list.')
+        else:
+            print("Task is not found in list.")
+    elif action.strip().lower() == "exit":
+        print("Goodbye.")
         break
     else:
-        print("what did you just put")
+        print("That is not a valid option.")
