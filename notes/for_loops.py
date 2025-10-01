ca036e87895a38f5d72cb341f8e13df28a90b38b
Valerie5801 <valerie.yang@ucas-edu.net>
2025-10-01 10:39:05 -0600

    Added fancy things to the notes

--- notes/for_loops.py
+++ notes/for_loops.py
@@ -1,5 +1,8 @@
 #VY 2nd For Loops Notes
 
+import time
+
+
 nums = [6, 51, 61, 94, 351, 946, 5489, 4, 654, 684]
 
 for num in nums:
@@ -9,17 +12,20 @@ for num in nums:
     else:
         print(num)
 
-print("WONDERHOY")
+print("WONDERHOY\n")
+time.sleep(3)
 
 for i in range(1, 10): #Range is not inclusive, so it leaves out 10
     print(i)
+time.sleep(3)
 
-
-print("count by twos")
+print("\ncount by twos")
 for x in range(2, 11, 2): #The third argument tells the computer what to count by
     print(x)
+time.sleep(3)
 
-
-print("Nwod tnuoc") #Counting down. The default for counting is 1. To count backwards you do -1.
+print("\nNwod tnuoc") #Count down. The default for counting is 1. To count backwards you do -1.
 for i in range(10, 0, -1):
     print(i)
+    time.sleep(1)
+print("happy new year or 5th annversary who am i kidding only the latter is here")
