51d664361268779e6de2fcc1c31ce63727e52eb2
Valerie5801 <valerie.yang@ucas-edu.net>
2025-10-02 10:35:07 -0600

    Finished the Multiplication table practice

--- Practices/multiplication_table.py
+++ Practices/multiplication_table.py
@@ -1,6 +1,11 @@
 #VY 2nd Multiplication Table
 
-for i in range(1, 13):
-    for x in range(1, 13):
-        result = i * x
-        print(result)
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+row = []
+
+for num in nums:
+    for i in range(1, 13):
+        result = i * num
+        row.append(result)
+    print(row)
+    row = []
