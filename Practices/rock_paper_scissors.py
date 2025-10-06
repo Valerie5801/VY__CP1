33fc34807b83c0008b5e6e74c8c07d3267d90338
Valerie5801 <valerie.yang@ucas-edu.net>
2025-10-06 10:43:02 -0600

    Polished Rock Paper Scissors practice

--- Practices/rock_paper_scissors.py
+++ Practices/rock_paper_scissors.py
@@ -9,6 +9,8 @@ while True:
         break
     elif play == "y":
         print("Let's begin.")
+        player_score = 0
+        comp_score = 0
         while True:
             player_choice = input("Choose rock, paper, or scissors: ")
             if player_choice != "rock" and player_choice != "paper" and player_choice != "scissors":
@@ -21,35 +23,47 @@ while True:
                 if comp_choice == "rock":
                     if player_choice.strip().lower() == "rock":
                         print("It's a tie.")
+                        print(f" Player score: {player_score}\n Computer score: {comp_score}")
                     elif player_choice.strip().lower() == "paper":
                         print("You win!")
+                        player_score += 1
+                        print(f" Player score: {player_score}\n Computer score: {comp_score}")
                     elif player_choice.strip().lower() == "scissors":
                         print("You lost.")
+                        comp_score += 1
+                        print(f" Player score: {player_score}\n Computer score: {comp_score}")
                         
                 elif comp_choice == "paper":
                     if player_choice.strip().lower() == "rock":
                         print("You lost.")
+                        comp_score += 1
+                        print(f" Player score: {player_score}\n Computer score: {comp_score}")
                     elif player_choice.strip().lower() == "paper":
                         print("It's a tie.")
+                        print(f" Player score: {player_score}\n Computer score: {comp_score}")
                     elif player_choice.strip().lower() == "scissors":
                         print("You win!")
+                        player_score += 1
+                        print(f" Player score: {player_score}\n Computer score: {comp_score}")
 
                 elif comp_choice == "scissors":
                     if player_choice.strip().lower() == "rock":
                         print("You win!")
+                        player_score += 1
+                        print(f" Player score: {player_score}\n Computer score: {comp_score}")
                     elif player_choice.strip().lower() == "paper":
                         print("You lost.")
+                        comp_score += 1
+                        print(f" Player score: {player_score}\n Computer score: {comp_score}")
                     elif player_choice.strip().lower() == "scissors":
                         print("It's a tie.")
+                        print(f" Player score: {player_score}\n Computer score: {comp_score}")
 
                 play_again = input("Play again?(y/n): ")
                 if play_again == "y":
                     continue
                 elif play_again == "n":
-                    print("Thank you for playing.")
+                    print(f"Thank you for playing. \nFinal scores are: \n\tPlayer score: {player_score} \n\tComputer score: {comp_score}")
                     break
-
-        if play_again == "n":
-            break
     else:
         print("That isn't a valid option.")
