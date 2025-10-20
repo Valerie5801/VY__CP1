72d004fb12e9bfe8dfd0139c4d4446f9c96529b8
Valerie5801 <valerie.yang@ucas-edu.net>
2025-10-20 10:50:58 -0600

    Finished Function Notes

---
+++ notes/functions.py
@@ -0,0 +1,13 @@
+#VY 2nd Functions Notes
+num = 0
+def add(x, y):
+    return x+y
+
+while num < add(1, 3):
+    print("wonder")
+    num+=1
+print("hoy")
+print(f"Your number is: {add(5, 3)}")
+total = add(8, 13) #setting functions as a variable
+print(add(add(3, 2), 10)) #using the result of a function as another function's parameter
+add(32429865743985,98547285)
