4f58ac3408d71fb79d83cb73eb5393b11e5eb6bc
Valerie5801 <valerie.yang@ucas-edu.net>
2025-10-21 10:50:46 -0600

    Started the Caesar Cipher Practice

---
+++ Practices/caesar_cipher.py
@@ -0,0 +1,9 @@
+#VY 2nd Caesar Cipher
+#Create two functions: One will be used for decoding and one will be used for encoding.
+    #Have two parameters for the encoding function: the shift value and the message.
+    #Have two parameters for the decoding function: the shift value and the encoded message.
+#Create an input asking for the user if they want to encode or decode.
+#Make an if statement checking if they want to encode or decode.
+    #If they want to encode, get the user input for the shift value and the message. Then use those two values as the arguments for the parameters in the function.
+        #After getting the values, check
+    #If they want to decode, get the user's input for the shift value and the message, then use those two values as arguments for the parameters.
--- notes/functions.py
+++ notes/functions.py
@@ -1,8 +1,28 @@
 #VY 2nd Functions Notes
-num = 0
+#all imports
+num = 0 #Global variables
+player_hp = 100
+monster_hp = 100
+
+#functions
 def add(x, y):
     return x+y
 
+def initials(name):
+    names = name.split(" ")
+    initial = ""
+    for name in names:
+        initial += name[0]
+    return initial
+
+def attack(dmg, turn):
+    if turn == "player":
+        return monster_hp - dmg, player_hp
+    else:
+        return monster_hp, player_hp - dmg
+
+
+#rest of the code
 while num < add(1, 3):
     print("wonder")
     num+=1
@@ -11,3 +31,17 @@ print(f"Your number is: {add(5, 3)}")
 total = add(8, 13) #setting functions as a variable
 print(add(add(3, 2), 10)) #using the result of a function as another function's parameter
 add(32429865743985,98547285)
+
+print(f"Tia's initials are: {initials("Tia LaRose")}")
+
+monster_hp, player_hp = attack(15, "player")
+print(f"Player Health: {player_hp}")
+print(f"Monster Health: {monster_hp}")
+
+monster_hp, player_hp = attack(15, "monster")
+print(f"Player Health: {player_hp}")
+print(f"Monster Health: {monster_hp}")
+
+#ASCII
+print(f"a = {ord("a")}")
+print(f"100 = {chr(100)}")
