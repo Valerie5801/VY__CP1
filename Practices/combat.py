7ad2cc9b9a7b8b772eef11e8d918bf46947cfd68
Valerie5801 <valerie.yang@ucas-edu.net>
2025-10-28 01:25:29 +0000

    Fixed the Combat Practice program

--- Practices/combat.py
+++ Practices/combat.py
@@ -4,22 +4,22 @@ import time
 
 fighters = {
     "Knight":{
-        "option_num" : 1,
         "health" : 30,
         "defense" : 4,
-        "attack" : 5 ,
+        "attack" : 5,
+        "guard" : False
     },
     "Mage":{
-        "option_num" : 2,
         "health" : 25,
         "defense" : 4,
         "attack" : 4,
+        "guard" : False
     },
     "Rogue":{
-        "option_num" : 3,
-        "health" : 15,
+        "health" : 20,
         "defense" : 3,
         "attack" : 6,
+        "guard" : False
     }
 }
 
@@ -32,34 +32,37 @@ enemies = {
     "Slime":{
         "health" : 15,
         "defense" : 3,
-        "attack" : 3,
+        "attack" : 5,
     },
     "Wolf":{
         "health" : 25,
-        "defense" : 5,
-        "attack" : 4,
+        "defense" : 4,
+        "attack" : 5,
     },
 }
 
 def user_combat():
+    time.sleep(1)
     print("Your turn.")
-    print("1. Normal Attack \n2. Wild Attack(you take a little bit of damage) \n3. Heal(by 6 points) \n4. Skip.")
+    print("1. Normal Attack \n2. Wild Attack(you take a little bit of damage) \n3. Heal(by 6 points) \n4. Guard.")
+    time.sleep(1)
     user_action = input("What would you like to do?: ")
     if user_action == "1":
-        damage = user_fighter["attack"] - enemy_fighter["defense"]
+        damage = max(0, user_fighter["attack"] - enemy_fighter["defense"])
         enemy_fighter["health"] -= damage
         if enemy_fighter["health"] < 0:
-            enemy_fighter["health"] == 0
+            enemy_fighter["health"] = 0
         print(f"You did a normal attack. {enemy_choice} now has {enemy_fighter["health"]} health.\n")
         time.sleep(1.5)
     elif user_action == "2":
         recoil = random.randint(2, 3)
-        enemy_fighter["health"] -= user_fighter["attack"]
+        damage = max(0, (user_fighter["attack"] * 1.5) - enemy_fighter["defense"])
         user_fighter["health"] -= recoil
-        if user_fighter["health"] < 0:
-            user_fighter["health"] == 0
+        if damage < 0:
+            damage = 0
+        enemy_fighter["health"] -= damage
         if enemy_fighter["health"] < 0:
-            enemy_fighter["health"] == 0
+            enemy_fighter["health"] = 0
         print(f"You did a wild attack. {enemy_choice} now has {enemy_fighter["health"]} health. \nHowever, you took {recoil} damage. You now have {user_fighter["health"]} health.\n")
         time.sleep(1.5)
     elif user_action == "3":
@@ -67,25 +70,31 @@ def user_combat():
         print(f"You healed yourself by 6 points. You now have {user_fighter["health"]} points of health left.\n")
         time.sleep(1.5)
     elif user_action == "4":
-        print("You skipped your turn.\n")
+        user_fighter["guard"] = True
+        print("You guarded yourself.\n")
         time.sleep(1.5)
     return user_fighter, enemy_fighter
 
 def enemy_combat():
     print(f"{enemy_choice}'s turn.")
     damage = enemy_fighter["attack"] - user_fighter["defense"]
+    if user_fighter["guard"]:
+        damage -= 2
+        user_fighter["guard"] = False
     user_fighter["health"] -= damage
     if user_fighter["health"] < 0:
-        user_fighter["health"] == 0
-    print(f"{enemy_choice} attacks you. \nYou now have {user_fighter["health"]} health.\n")
+        user_fighter["health"] = 0
     time.sleep(1.5)
+    print(f"{enemy_choice} attacks you. \nYou now have {user_fighter["health"]} health.\n")
     return user_fighter
 
 print("Hello! This a combat training simulator.")
+time.sleep(0.5)
 name = input("First off, what is your name?: ")
 print(f"Nice to meet you, {name}! \nNow, here are your options for the classes: \n1 for a Knight \n2 for a Mage \n3 for a Rogue.")
 user_fighter = input("Choose a class: ")
 print("Awesome! Now...\n")
+time.sleep(1)
 
 if user_fighter == "1":
     user_fighter = fighters["Knight"]
