bceb050086a88093a8b9ee36ecdd536be8e0af47
Valerie5801 <valerie.yang@ucas-edu.net>
2025-11-03 10:50:46 -0700

    continued the maze generator

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -3,30 +3,31 @@ import turtle #import turtle here
 import random
 
 #make a list of characters that look like walls as a placeholder
-walls = ["|", "_"]
+walls = ["|", "_", " "]
 
 #make a list of characters with blank spaces as the places for where the things go
-columns = [" ", " ", " ", " ", " ", " "] #this has six values for the six rows.
+#columns = [" ", " ", " ", " ", " ", " "] #this has six values for the six rows.
 
-#Make a function for the maze generation 
-def maze_gen(): #make a nested for loop for the square grid.
-    rows = [] #make a list for the rows. This is for the grid.
-    for column in columns:
-        for i in range(1, 7):
-            row = walls[random.randint(0, 1)]
-            rows.append(column)
-            return rows
-        #print(columns)
-        #rows = []
-        
 
-    
-maze_gen()
+rows = [
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), choose_wall],
+    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall],
+    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall],
+    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall],
+    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall],
+    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall]
+]
+#make a function for the maze generation
+def maze_gen():
+    for row in rows:
+        for spot in rows:
+            if spot:
+                print("I STILL HATE EVERYTHING")
+                continue
+            else:
+                print("I HATE EVERYTHING")
+
+#def is_solvable(row_grid, col_grid):
+ #   size = len(row_grid) - 1
+
 #When having turtle draw the maze, make it do pendown, penup, at certain points to make the maze.
-
-turtle.penup()
-turtle.goto(x=-350, y=250)
-turtle.pensize(5)
-turtle.pendown()
-
-turtle.done()
