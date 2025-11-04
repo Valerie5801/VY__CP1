7223e0b7f8183434e1d0b2f0fb973692cfb02637
Valerie5801 <valerie.yang@ucas-edu.net>
2025-11-03 19:43:06 -0700

    started to create a function for the maze generation

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -2,31 +2,60 @@
 import turtle #import turtle here
 import random
 
-#make a list of characters that look like walls as a placeholder
-walls = ["|", "_", " "]
-
-#make a list of characters with blank spaces as the places for where the things go
-#columns = [" ", " ", " ", " ", " ", " "] #this has six values for the six rows.
-
-
+#make two lists inside of a list; one for the rows and one for the columns. use random.randint to for 0 or 1 (no wall or wall.)
 rows = [
-    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), choose_wall],
-    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall],
-    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall],
-    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall],
-    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall],
-    [choose_wall, choose_wall, choose_wall, choose_wall, choose_wall, choose_wall]
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)]
 ]
-#make a function for the maze generation
-def maze_gen():
-    for row in rows:
-        for spot in rows:
-            if spot:
-                print("I STILL HATE EVERYTHING")
-                continue
-            else:
-                print("I HATE EVERYTHING")
 
+columns = [
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)],
+    [random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2), random.randint(0, 2)]
+]
+
+#create a function for the maze generation.
+def maze_gen():
+    count_col = 0
+    count_row = 0
+    turtle.penup()
+    turtle.goto(x=-250, y=-250)
+    for row in rows:
+        for spot in row:
+            if spot > 0:
+                turtle.forward(25)
+                turtle.penup()
+            else:
+                turtle.penup()
+                turtle.forward(25)
+        count_row += 1
+        turtle.goto(x=-250, y=-250+(count_row*25))
+        turtle.pendown()
+    turtle.goto(x=-250, y=0)
+    turtle.right(90)
+    for column in columns:
+        for spot in column:
+            if spot > 0:
+                turtle.forward(25)
+                turtle.penup()
+            else:
+                turtle.penup()
+                turtle.forward(25)
+        count_col += 1
+        turtle.goto(x=-250+(count_col*25), y=-150)
+        turtle.pendown()
+
+
+turtle.screensize(canvwidth=500, canvheight=500)
+maze_gen()
+turtle.done()
 #def is_solvable(row_grid, col_grid):
  #   size = len(row_grid) - 1
 
