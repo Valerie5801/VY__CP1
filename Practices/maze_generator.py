dc36b6850eb243b8add04e3d25a764ff2f60b1f2
Valerie5801 <valerie.yang@ucas-edu.net>
2025-11-06 10:50:28 -0700

    Added more to the maze generator project

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -2,28 +2,51 @@
 import turtle #import turtle here
 import random
 
-#make two lists inside of a list; one for the rows and one for the columns. use random.randint to for 0 or 1 (no wall or wall.)
-rows = [
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)]
+#make two lists inside of a list; one for the rows and one for the columns. make each list inside of the list empty so we can randomize them (for the walls) any time we want. These are like blank slates. This square will be a 12x12 because of the size of the square, and by doing a 6x6 and the nested for loop run twice will make the maze be repetitive.
+rows = [ #12 lists inside of each list because its a 12x12
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
 ]
+
 
 columns = [
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-    [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)]
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
 ]
 
+#make a function to put random values inside of the rows and columns lists
+def maze_gen(row_grid, col_grid):
+    for row in row_grid:
+        for i in range(12): #since this is a 12x12 square, this must run 12 times to fill all spots
+            row.append(random.randint(0, 1))
+    for column in col_grid:
+        for i in range(12): #same thing here
+            column.append(random.randint(0, 1))
+    return row_grid, col_grid
+
 #create a function to check if the maze is solvable.
-def is_solvable(row_grid, col_grid):
-    size = len(row_grid) - 1
+def is_solvable(grid):
+    size = len(grid[0]) - 1
     visited = set() #empty list for coordinates that have already been visited
     stack = [(0, 0)]
     
@@ -33,65 +56,63 @@ def is_solvable(row_grid, col_grid):
         if x == size - 1 and y == size - 2:
             return True
         
-        if (x, y) in visited:
+        if (x, y) in visited: #check if we've been here before
             continue
 
-        visited.add((x, y))
+        visited.add((x, y)) #add this spot to the "we've been here before" list
 
-        if x < size - 1 and col_grid[y][x+1] == 0: #check the spot to the right for a wall
+        if x < size and grid[1][x+1][y] == 0: #check the spot to the right for a wall
             stack.append((x+1, y))
 
-        if y < size - 1 and row_grid[y][x+1] == 0: #check the spot upwards for a wall
+        if y < size - 1 and grid[0][x][y+1] == 0: #check the spot upwards for a wall
             stack.append((x, y+1))
         
-        if x > 0 and col_grid[y][x] == 0: #check the spot leftwards for a wall
+        if x > 0 and grid[1][x][y] == 0: #check the spot leftwards for a wall
             stack.append((x-1, y))
         
-        if y > 0 and row_grid[y][x] == 0: #check the spot downwards for a wall
+        if y > 0 and grid[0][x][y] == 0: #check the spot downwards for a wall
             stack.append((x, y-1))
 
     return False #return false if we have checked every index and there is no exit
 
 #create a function for the maze generation.
-def draw_maze():
-    turtle.speed(0.5)
+def draw_maze(grid):
+    turtle.speed(0)
     turtle.penup()
     turtle.goto(x=0, y=25) #go near the bottom left corner (not the bottom since we don't want it to draw over the entrance and face right to start drawing rows.
     count_row = 0 #counter for how many rows the turtle has completed
     count_col = 0 #counter for how many columns the turtle has completed
-    for i in range(2): #make a nested loop for drawing rows
-        for row in rows:
-            for spot in row:
-                if spot == 0: #don't draw if that spot is a 0
-                    turtle.penup()
-                    turtle.forward(50)
-                elif spot == 1: #draw if that spot is a 1
-                    turtle.pendown()
-                    turtle.forward(50)
-                    turtle.penup()
-            count_row += 1 #add one for each row the turtle completes
-            turtle.goto(x=0, y=0+(count_row*25))
+    for row in grid[0]: #make a nested loop for drawing rows
+        for spot in row:
+            if spot == 0: #don't draw if that spot is a 0
+                turtle.penup()
+                turtle.forward(25)
+            elif spot == 1: #draw if that spot is a 1
+                turtle.pendown()
+                turtle.forward(25)
+                turtle.penup()
+        count_row += 1 #add one for each row the turtle completes
+        turtle.goto(x=0, y=25+(count_row*25))
     turtle.penup()
-    turtle.goto(x=0, y=300) #go to the top left corner and face down to start drawing columns
-    turtle.right(90)
-    for i in range(2): #make a nested for loop for drawing the columns.
-        for column in columns:
-            for spot in column:
-                if spot == 0: #don't draw if spot is 0
-                    turtle.penup()
-                    turtle.forward(50)
-                elif spot == 1: #draw if spot is a 1
-                    turtle.pendown()
-                    turtle.forward(50)
-                    turtle.penup()
-            count_col += 1 #add one for each column the turtle completes
-            turtle.goto(x=0+(count_col*25), y=300)
+    turtle.goto(x=0, y=0) #go to the bottom left corner and face up to start drawing columns
+    turtle.right(-90)
+    for column in grid[1]: #make a nested for loop for drawing the columns.
+        for spot in column:
+            if spot == 0: #don't draw if spot is 0
+                turtle.penup()
+                turtle.forward(25)
+            elif spot == 1: #draw if spot is a 1
+                turtle.pendown()
+                turtle.forward(25)
+                turtle.penup()
+        count_col += 1 #add one for each column the turtle completes
+        turtle.goto(x=0+(count_col*25), y=0)
 
 
 def final_maze():
     while True:
-        if is_solvable(rows, columns):
-            draw_maze()
+        if is_solvable(maze_gen(rows, columns)):
+            draw_maze(maze_gen(rows, columns))
             break
         else:
             continue
@@ -110,5 +131,5 @@ for i in range(4): #make a loop that makes a square
     else: #if not, just draw a straight line.
         turtle.forward(275)
     turtle.right(-90)
-draw_maze()
+final_maze()
 turtle.done()
