765530e50668668608fca608563f2df3daea587d
Valerie5801 <valerie.yang@ucas-edu.net>
2025-11-10 10:50:56 -0700

    Started pseudocoding for the order up practice

--- Practices/order.py
+++ Practices/order.py
@@ -1,2 +1,16 @@
 #VY 2nd Order Up
 
+#create four dictionaries, one for each of these categories: drinks, sides, and main meals
+#each dictionary must have at least 4 different menu items(eight for sides, since the user needs to order two side dishes), and each menu item must have a cost. Do a nested dictionary for this.
+
+#greet the user
+#print the full menu by looping through the dictionary
+#afterwards, ask the user what drink they would like
+    #if the drink is not found in the dictionary, tell them to order again
+#ask the user what their first side is
+    #if the side is not found in the dictionary, tell them to order again.
+#ask the user what their second side is, or if they don't want another side.
+    #if the side is not found in the dictionary, tell them to order again.
+    #if they don't want a side, don't find it in the dictionary.
+
+#get all of the items that the user ordered, get their price (and if we can, tax them >:])
