e3aaa83041e0bef0c73ad5ab22419b333714cf5a
Valerie5801 <valerie.yang@ucas-edu.net>
2025-11-12 10:50:45 -0700

    Finished the Order Up practice

--- Practices/order.py
+++ Practices/order.py
@@ -1,5 +1,5 @@
 #VY 2nd Order Up
-
+import time
 #create four dictionaries, one for each of these categories: drinks, sides, and main meals
 #each dictionary must have at least 4 different menu item, and each menu item must have a cost. Do a nested dictionary for this.
 drinks = {
@@ -9,17 +9,17 @@ drinks = {
     "Sprite": 0.50,
     "Fanta": 0.80
 }
-sides = {
-    "Fries": 3.50,
-    "Pretzels": 3.99,
-    "Chips": 1.99,
-    "Salad": 2.99,
-}
 mains = {
     "Pizza": 5.99,
     "Cheeseburger": 7.99,
     "Hamburger": 8.99,
     "Chicken tenders": 6.99
+}
+sides = {
+    "Fries": 3.50,
+    "Pretzels": 3.99,
+    "Chips": 1.99,
+    "Salad": 2.99,
 }
 
 #print the full menu by looping through the dictionary. Make a function for this, and call it if the user wants to see the menu.
@@ -28,90 +28,122 @@ def view_menu():
     for drink in drinks.keys():
         print(f"\t{drink} is {drinks[drink]}")
 
-    print("Sides:")
-    for side in sides.keys():
-        print(f"\t{side} is {sides[side]}")
-    
     print("Main meals:")
     for meal in mains.keys():
         print(f"\t{meal} is {mains[meal]}")
 
+    print("Sides:")
+    for side in sides.keys():
+        print(f"\t{side} is {sides[side]}")
 
-#make a function for ordering.
-def ordering():
+
+#make a function for ordering drinks
+def order_drink():
     while True:
         user_drink = input("What drink would you like?(n if you're not ready): ")
         #if the item is on a different dictionary, print something else and tell them to order again.
         if user_drink.capitalize() in sides or user_drink.capitalize() in mains:
             print("Please choose a drink as of right now.")
-        #if the drink is not found in the dictionary, tell them to order again
-        elif user_drink.capitalize() not in drinks:
-            print("Sorry, that is not on our menu. Please choose an available option.")
-            #give the user the option to say they aren't ready yet.
+        #give the user the option to say they aren't ready yet.
         elif user_drink == "n":
             print("Let me know when you are ready to order.")
             break
+        #if the drink is not found in the dictionary, tell them to order again
+        elif user_drink.capitalize() not in drinks:
+            print("Sorry, that is not on our menu. Please choose an available option.")
         else:
-            break
+            return user_drink
 
-    #ask the user what main they want
+#make a function for ordering a main
+def order_main():
     while True:
         user_main = input("What main meal do you want?(n if you're not ready): ")
         #if the item is on a different dictionary, print something else and tell them to order again.
         if user_main.capitalize() in sides or user_main.capitalize() in drinks:
             print("Please choose a main meal as of right now.")
-        elif user_main.capitalize() not in mains:
-            print("Sorry, that is not on our menu. Please choose an available option.")
-            #give the user the option to say they aren't ready yet.
+        #give the user the option to say they aren't ready yet.
         elif user_main == "n":
             print("Alright! Let me know when you're ready to order.")
             break
+        elif user_main.capitalize() not in mains:
+            print("Sorry, that is not on our menu. Please choose an available option.")
         else:
-            break
+            return user_main
 
-    #ask the user what their first side is
+#make a function for ordering sides
+def first_side():
+    two_sides = False
     while True:
         first_side = input("What will your first side be?(n if you're not ready): ")
         #if the item is on a different dictionary, print something else and tell them to order again.
         if first_side.capitalize() in drinks or first_side.capitalize() in mains:
             print("Please choose a side as of right now.")
-        #if the side is not found in the dictionary, tell them to order again.
-        elif first_side.capitalize() not in sides:
-            print("Sorry, that is not on our menu. Pleaes chooes an available option.")
         #give the user the option to say they aren't ready yet.
         elif first_side == "n":
             print("Okay. Let me know when you want to order.")
-            break
+            continue
+        #if the side is not found in the dictionary, tell them to order again.
+        elif first_side.capitalize() not in sides:
+            print("Sorry, that is not on our menu. Please choose an available option.")
         else:
-            break
+            return first_side
 
+def second_side():
     #ask the user if they want a second one.
     ask_second = input("Would you like a second side?(y/n): ")
     if ask_second == "y":
         while True:
-            second_side = input("What will you rsecond side be?(n if you're not ready): ")
+            second_side = input("What will your second side be?(n if you're not ready): ")
             #if the item is on a different dictionary, print something else and tell them to order again.
             if second_side.capitalize() in drinks or second_side.capitalize() in mains:
                 print("Please choose a side as of right now.")
-            #if the side is not found in the dictionary, tell them to order again.
-            elif second_side.capitalize() not in sides:
-                print("Sorry, that is not on our menu. Pleaes chooes an available option.")
             #give the user the option to say they aren't ready yet.
-            elif second_side.capitalize() == "n":
+            elif second_side == "n":
                 print("Let me know when you know what you want.")
                 break
+            #if the side is not found in the dictionary, tell them to order again.
+            elif second_side.capitalize() not in sides:
+                print("Sorry, that is not on our menu. Please choose an available option.")
             else:
-                break
+                return second_side
     elif ask_second == "n":
         print("Alright then.")
+        return None
+        
+#make a function for adding up all values
+def add_total(drink, main, one_side, second_side):
+    total = drinks[drink] + mains[main] + sides[one_side]
+    if sides[second_side]:
+        total += sides[second_side]
+    return total
+
 
 #greet the user and show them the menu.
 print("Hello there!")
-print("Here is our menu.\n")
-view_menu()
 
+while True:
+    #ask the user if they're ready to order or not yet
+    user_choice = input("Do you want to order or see the menu?(o/m): ")
+    if user_choice == "o":
+        #get all of the items(the keys) that the user ordered, get their price (the values) and add it all up
+        drink_choice = order_drink()
+        main_choice = order_main()
+        first_side_choice = first_side()
+        second_side_choice = second_side()
+        if second_side_choice:
+            final_total = add_total(drink_choice, main_choice, first_side_choice, second_side_choice)
+            print(f"Your order: \n\tDrink: {drink_choice} \n\tMain Course: {main_choice} \n\tSide Dishes: \n\t\t{first_side_choice} \n\t\t{second_side_choice} \nTotal Cost: ${final_total}")
+        else:
+            final_total = add_total(drink_choice, main_choice, first_side_choice, 0)
+            print(f"Your order: \n\tDrink: {drink_choice} \n\tMain Course: {main_choice} \n\tSide Dishes: \n\t\t{first_side_choice} \nTotal Cost: ${final_total}")
+        break
+    elif user_choice == "m":
+        print("Here is our menu.")
+        view_menu()
+        time.sleep(3)
+    else:
+        print("Sorry, I didn't get that.")
 
-#get all of the items(the keys) that the user ordered, get their price (the values) and add it all up
 
 #finally, print all of the items(set as keys) and print out the price of the order.
 
