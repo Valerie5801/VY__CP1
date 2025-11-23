34542b2fdd5ca74144683e3ee3b0ab464afe86fc
Valerie5801 <valerie.yang@ucas-edu.net>
2025-11-22 19:05:22 -0700

    Fixed the Factorial calculator.

--- Practices/factorial.py
+++ Practices/factorial.py
@@ -1,7 +1,7 @@
 #VY 2nd Factorial Calculator
 
 #ask user what they want the factorial of
-user_input = input("What number do you want the factorial of?: ")
+#user_input = input("What number do you want the factorial of?: ")
 #add line 1 to list                       #You mean the first line? It's kinda unclear.
 #series_nums = [user_input]               #I can't name a list, "list" as it's variable, so I came up with my own name
                                           #Also why do we need a list??
@@ -12,7 +12,7 @@ user_input = input("What number do you want the factorial of?: ")
         #total *= number
         #number -= 1
         #list.append(number)
-def get_factorial():
+def get_factorial(usr_num):               #made it take in a parameter for modularity
     number = int(user_input)              #Must be single equals sign in order for it to be the assignment sign
     total = 1                             #Also must be single equals sign in order for it to be the assignment sign
     while number > 0:                     #This must be strictly if the number is greater than zero to prevent multiplying the total by 0, which will cause the result to always be 0.
@@ -22,8 +22,19 @@ def get_factorial():
     return total                          #We have to return/print something from a function!
 
 
-#get_factorial
-print(f"It's factorial is...{get_factorial()}") #I had to rephrase.
+print("This is a factorial calculator.")
+
+while True:                               #While True loop so user can do as many factorials as they want.
+    #ask user what they want the factorial of
+    user_input = input("What number do you want the factorial of?(limit of 3 digits): ") #I moved this down here to make the code easier to read and for organization.
+
+    #get_factorial
+    print(f"The factorial of {user_input} is...{get_factorial(user_input)}") #I had to rephrase.
+
+    another_factor = input("Do you want to get the factorial of another number?(y/n): ")
+    if another_factor == "n":
+        print("Thank you for using the factorial calculator.")
+        break
 
 #for i in list print "i":                 #What is this for loop's purpose??
     #print("equals", total)
