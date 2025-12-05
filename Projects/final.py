a39596b3f0fa7c978bef19ea9144540d17a7d8c0
Valerie5801 <valerie.yang@ucas-edu.net>
2025-12-05 10:50:35 -0700

    Continued combat functions

--- Projects/final.py
+++ Projects/final.py
@@ -10,7 +10,7 @@ user_stats = {
     "Attack": 5,
     "Defense": 2,
     "Guard": False,
-    "Charge_counter": 0
+    "Charge counter": 0
 }
 
 spirit_stats = {
@@ -21,11 +21,11 @@ spirit_stats = {
 }
 
 boss_stats = {
-    "Type": "Boss",
+    "Type": "Warden",
     "Health": 70,
     "Attack": 4,
     "Defense": 4,
-    "Charge_counter": 0
+    "Charge counter": 0
 }
 
 items = {
@@ -72,3 +72,100 @@ items = {
         "Room": 7
     }
 }
+
+#Combat functions
+def user_combat(enemy_stats):
+    print("Your turn.")
+    if enemy_stats["Type"] == "Spirit":
+        print("1. Attack \n2. Flee \n 3. Expel \n4. Guard")
+    elif enemy_stats["Type"] == "Warden":
+        print("1. Attack \n2. Flee \n 3. Charged Attack \n4. Guard")
+    
+    while True:
+        user_action = input("What would you like to do?: ")
+
+        if user_action == "1":
+            damage = user_stats["Attack"] - enemy_stats["Defense"]
+            enemy_stats["Health"] -= damage
+            if enemy_stats["Health"] < 0:
+                enemy_stats["Health"] == 0
+            print(f"You attacked. {enemy_stats["Type"]} took {damage} damage. It now has {enemy_stats["Health"]} health left.")
+            break
+        elif user_action == "2":
+            flee_chance = random.randint(1, 10)
+            if flee_chance <= 3: #add a way for the user to back out of the final boss battle and return them to the previous room.
+                enemy_stats["Health"] = -1
+                print("You successfully ran away.")
+            else:
+                print("You failed to run away.")
+            break
+        elif user_action == "3":
+            if enemy_stats["Type"] == "Spirit":
+                print("You try to erase the spirit from existence.")
+                expel_chance = random.randint(1, 10)
+                if expel_chance == 5:
+                    enemy_stats["Health"] = 0
+                    print("You expelled the spirit.")
+                else:
+                    print("You failed to expel the spirit.")
+                break
+            elif enemy_stats["Type"] == "Warden" and user_stats["Charge counter"] <= 3:
+                damage = (user_stats["Attack"] + 4) - enemy_stats["Defense"]
+                enemy_stats["Health"] -= damage
+                print(f"You do a charged attack. Warden has {enemy_stats["Health"]} health left.")
+                user_stats["Charge counter"] += 1
+                break
+            else:
+                print("You cannot do any more charge attacks.")
+        elif user_action == "4":
+            user_stats["Guard"] = True
+            print("You guarded yourself.")
+            break
+
+    return user_stats, enemy_stats
+
+
+def spirit_combat(enemy_stats):
+    print("Spirit's turn.")
+    damage = enemy_stats["Attack"] - user_stats["Defense"]
+    if user_stats["Guard"]:
+        damage -= 2
+        user_stats["Guard"] = False
+
+    user_stats["Health"] -= damage
+    if user_stats["Health"] < 0:
+        user_stats["Health"] = 0
+    
+    print(f"Spirit attacks you for {damage} damage. You now have {user_stats["Health"]} health left.")
+    return user_stats, enemy_stats
+
+
+def boss_combat():
+    print("Warden's turn.")
+    boss_action = random.randint(1, 2)
+    if boss_stats["Charge counter"] <= 2:
+        boss_action = 1
+    
+    if boss_action == 1:
+        print("Warden does a normal attack.")
+        damage = boss_stats["Attack"] - user_stats["Defense"]
+        if user_stats["Guard"]:
+            damage -= 2
+            user_stats["Guard"] = False
+    elif boss_action == 2:
+        print("Warden does a charged attack.")
+        damage = boss_stats["Attack"]
+        if user_stats["Guard"]:
+            damage -= 2
+            user_stats["Guard"] = False
+        boss_stats["Charge counter"] += 1
+
+    user_stats["Health"] -= damage
+    print(f"Warden did {damage} damage. You now have {user_stats["Health"]} health left.")
+    return user_stats, boss_stats
+
+
+def main_battle(enemy):
+    print(f"{enemy["Type"]} appeared!")
+    going_first = random.randint(1, 2)
+    
